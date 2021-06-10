870010d5347ae3e6794df308679911ccd9c72113
toyobayashi <lifenglin314@outlook.com>
2021-06-10 14:15:57 +0800

    napi_typedarray_type

--- lib/runtime/type.ts
+++ lib/runtime/type.ts
@@ -56,4 +56,18 @@ namespace emnapi {
     napi_external,
     napi_bigint
   }
+
+  export enum napi_typedarray_type {
+    napi_int8_array,
+    napi_uint8_array,
+    napi_uint8_clamped_array,
+    napi_int16_array,
+    napi_uint16_array,
+    napi_int32_array,
+    napi_uint32_array,
+    napi_float32_array,
+    napi_float64_array,
+    napi_bigint64_array,
+    napi_biguint64_array
+  }
 }
