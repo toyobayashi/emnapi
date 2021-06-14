513b5f7b7631c6ab710afed10a3062c25986a920
toyobayashi <lifenglin314@outlook.com>
2021-06-14 13:41:18 +0800

    pass wrap test

---
+++ test/passwrap/passwrap.test.js
@@ -0,0 +1,25 @@
+/* eslint-disable symbol-description */
+/* eslint-disable camelcase */
+'use strict'
+// Flags: --expose-gc
+
+const assert = require('assert')
+const common = require('../common')
+const { load } = require('../util')
+
+const p = load('passwrap')
+p.then(addon => {
+  async function runTest () {
+    let obj1 = addon.createObject(10)
+    let obj2 = addon.createObject(20)
+    const result = addon.add(obj1, obj2)
+    assert.strictEqual(result, 30)
+
+    // Make sure the native destructor gets called.
+    obj1 = null
+    obj2 = null
+    await common.gcUntil('8_passing_wrapped',
+      () => (addon.finalizeCount() === 2))
+  }
+  runTest()
+})
