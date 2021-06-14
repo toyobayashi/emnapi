43a2809280268eb6c3346914fac7239e554234e0
toyobayashi <lifenglin314@outlook.com>
2021-06-14 14:14:55 +0800

    date test

---
+++ test/date/date.test.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-new-object */
+/* eslint-disable no-new-wrappers */
+/* eslint-disable camelcase */
+'use strict'
+const assert = require('assert')
+const { load } = require('../util')
+
+load('date').then(test_date => {
+  const dateTypeTestDate = test_date.createDate(1549183351)
+  assert.strictEqual(test_date.isDate(dateTypeTestDate), true)
+
+  assert.strictEqual(test_date.isDate(new Date(1549183351)), true)
+
+  assert.strictEqual(test_date.isDate(2.4), false)
+  assert.strictEqual(test_date.isDate('not a date'), false)
+  assert.strictEqual(test_date.isDate(undefined), false)
+  assert.strictEqual(test_date.isDate(null), false)
+  assert.strictEqual(test_date.isDate({}), false)
+
+  assert.strictEqual(test_date.getDateValue(new Date(1549183351)), 1549183351)
+})
