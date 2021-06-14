38c07a6dbc91219f34f6c86199873d8df41eb898
toyobayashi <lifenglin314@outlook.com>
2021-06-14 14:12:11 +0800

    dataview test

---
+++ test/dataview/dataview.test.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-new-object */
+/* eslint-disable no-new-wrappers */
+/* eslint-disable camelcase */
+'use strict'
+const assert = require('assert')
+const { load } = require('../util')
+
+load('dataview').then(test_dataview => {
+// Test for creating dataview
+  {
+    const buffer = new ArrayBuffer(128)
+    const template = Reflect.construct(DataView, [buffer])
+
+    const theDataview = test_dataview.CreateDataViewFromJSDataView(template)
+    assert.ok(theDataview instanceof DataView,
+            `Expect ${theDataview} to be a DataView`)
+  }
+
+  // Test for creating dataview with invalid range
+  {
+    const buffer = new ArrayBuffer(128)
+    assert.throws(() => {
+      test_dataview.CreateDataView(buffer, 10, 200)
+    }, RangeError)
+  }
+})
