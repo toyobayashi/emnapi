68eef415a1a2bd8ddb7870531639030f3c635db1
toyobayashi <lifenglin314@outlook.com>
2021-12-06 10:42:20 +0800

    move cgen config to test dir

--- test/util.js
+++ test/util.js
@@ -2,7 +2,7 @@ const { join } = require('path')
 const common = require('./common.js')
 
 function getEntry (targetName) {
-  return join(__dirname, `../.cgenbuild/${common.buildType}/${targetName}.${process.env.EMNAPI_TEST_NATIVE ? 'node' : 'js'}`)
+  return join(__dirname, `./.cgenbuild/${common.buildType}/${targetName}.${process.env.EMNAPI_TEST_NATIVE ? 'node' : 'js'}`)
 }
 
 exports.getEntry = getEntry
