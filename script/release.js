dff0e92f38f8db120a19e7423f6535867149d645
toyobayashi <lifenglin314@outlook.com>
2021-12-06 11:32:12 +0800

    release cmakelists

--- script/release.js
+++ script/release.js
@@ -18,5 +18,16 @@ fs.readdirSync(path.join(__dirname, '../include')).forEach(item => {
   }
 })
 
+fs.writeFileSync(path.join(root, 'CMakeLists.txt'), `cmake_minimum_required(VERSION 3.9)
+
+project(emnapi)
+
+add_library(emnapi STATIC "\${CMAKE_CURRENT_SOURCE_DIR}/src/emnapi.c")
+
+target_include_directories(emnapi PUBLIC "\${CMAKE_CURRENT_SOURCE_DIR}/include/emnapi")
+
+target_link_options(emnapi INTERFACE "--js-library=\${CMAKE_CURRENT_SOURCE_DIR}/lib/library_napi.js")
+`, 'utf8')
+
 crossZip.zipSync(root, path.join(__dirname, 'emnapi.zip'))
 fs.rmSync(root, { force: true, recursive: true })
