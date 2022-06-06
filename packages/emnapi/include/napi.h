4eb3a16a6db65e993093fedca05d72ddddd2a45b
toyobayashi <lifenglin314@outlook.com>
2022-06-06 18:27:11 +0800

    ArrayBuffer::IsDetached

--- packages/emnapi/include/napi.h
+++ packages/emnapi/include/napi.h
@@ -1117,10 +1117,10 @@ namespace NAPI_CPP_CUSTOM_NAMESPACE {
    void* Data();         ///< Gets a pointer to the data buffer.
    size_t ByteLength();  ///< Gets the length of the array buffer in bytes.
 
-// #if NAPI_VERSION >= 7
-//     bool IsDetached() const;
-//     void Detach();
-// #endif  // NAPI_VERSION >= 7
+#if NAPI_VERSION >= 7
+    bool IsDetached() const;
+    // void Detach();
+#endif  // NAPI_VERSION >= 7
   };
 
   /// A JavaScript typed-array value with unknown array type.
