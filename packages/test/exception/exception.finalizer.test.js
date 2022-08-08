18d9501296853831c2961449161304fc91d07628
toyobayashi <lifenglin314@outlook.com>
2022-08-08 23:18:31 +0800

    test: finalizer exception

---
+++ packages/test/exception/exception.finalizer.test.js
@@ -0,0 +1,35 @@
+/* eslint-disable camelcase */
+'use strict'
+const { load } = require('../util')
+// const common = require('../common')
+const assert = require('assert')
+// const { fork } = require('child_process')
+
+async function main () {
+  if (process.argv[2] === 'child') {
+    const loadPromise = load('exception')
+    try {
+      await loadPromise
+    } catch (anException) {
+      anException.binding.createExternal()
+    }
+
+    let gcCount = 10
+    ;(function gcLoop () {
+      global.gc()
+      if (--gcCount > 0) {
+        setImmediate(() => gcLoop())
+      }
+    })()
+    return
+  }
+
+  const { spawnSync } = require('child_process')
+  const child = spawnSync(process.execPath, [
+    '--expose-gc', ...(process.env.MEMORY64 ? ['--experimental-wasm-memory64'] : []), __filename, 'child'
+  ])
+  assert.strictEqual(child.signal, null)
+  assert.match(child.stderr.toString(), /Error during Finalize/)
+}
+
+module.exports = main()
