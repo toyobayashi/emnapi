4e70e3f149bf5e011e2b3255732e6fa87edbdb3b
toyobayashi <lifenglin314@outlook.com>
2023-01-01 20:12:24 +0800

    fix: ci

--- packages/emnapi/src/script.ts
+++ packages/emnapi/src/script.ts
@@ -1,10 +1,7 @@
-/* eslint-disable @typescript-eslint/no-redeclare */
 /* eslint-disable @typescript-eslint/indent */
 
-// #if DYNAMIC_EXECUTION
-
-// @ts-expect-error
 function napi_run_script (env: napi_env, script: napi_value, result: Pointer<napi_value>): napi_status {
+// #if DYNAMIC_EXECUTION
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   let value: number
 
@@ -24,15 +21,10 @@ function napi_run_script (env: napi_env, script: napi_value, result: Pointer<nap
     $makeSetValue('result', 0, 'value', '*')
     return envObject.getReturnStatus()
   })
-}
-
 // #else
-
-// @ts-expect-error
-function napi_run_script (env: napi_env, script: napi_value, result: Pointer<napi_value>): napi_status {
-  return _napi_set_last_error(env, napi_status.napi_generic_failure, 0, 0)
-}
-
+  const status = _napi_set_last_error(env, napi_status.napi_generic_failure, 0, 0)
 // #endif
+  return status
+}
 
 emnapiImplement('napi_run_script', 'ippp', napi_run_script, ['napi_set_last_error'])
