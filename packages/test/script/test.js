c2dd1ca0369775acc445c822e2cc004aba73709d
toyobayashi <lifenglin314@outlook.com>
2023-01-05 11:26:51 +0800

    test: add buffer test

--- packages/test/script/test.js
+++ packages/test/script/test.js
@@ -2,8 +2,9 @@ const { spawnSync } = require('child_process')
 const glob = require('glob')
 
 const cwd = require('path').join(__dirname, '..')
+const subdir = process.argv[2] || '**'
 
-const files = glob.sync('**/*.test.js', {
+const files = glob.sync(`${subdir}/*.test.js`, {
   cwd,
   ignore: process.env.EMNAPI_TEST_NATIVE
     ? ['**/{emnapitest,node-addon-api}/**/*']
