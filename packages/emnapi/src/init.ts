148db307de0538df994a6823b82cdaf83b9b1765
toyobayashi <lifenglin314@outlook.com>
2023-01-26 00:55:11 +0800

    refactor mergeInto

--- packages/emnapi/src/init.ts
+++ packages/emnapi/src/init.ts
@@ -26,64 +26,68 @@ declare interface InitOptions {
   nodeBinding?: NodeBinding
 }
 
-mergeInto(LibraryManager.library, {
-  $emnapiCtx: undefined,
-  $emnapiNodeBinding: undefined,
-  $emnapiModule: {
-    exports: {},
-    loaded: false,
-    filename: ''
-  },
-
-  $emnapiInit__deps: ['$emnapiModule', '$emnapiCtx', '$emnapiNodeBinding', 'napi_register_wasm_v1'],
-  $emnapiInit: function (options: InitOptions) {
-    if (emnapiModule.loaded) return emnapiModule.exports
-
-    if (typeof options !== 'object' || options === null) {
-      throw new TypeError('Invalid emnapi init option')
-    }
-
-    const context = options.context
-    if (typeof context !== 'object' || context === null) {
-      throw new TypeError("Invalid `options.context`. You can create a context by `import { createContext } from '@tybys/emnapi-runtime`'")
-    }
-
-    emnapiCtx = context
-
-    if (typeof options.filename === 'string') {
-      emnapiModule.filename = options.filename
-    }
-
-    if ('nodeBinding' in options) {
-      const nodeBinding = options.nodeBinding
-      if (typeof nodeBinding !== 'object' || nodeBinding === null) {
-        throw new TypeError('Invalid `options.nodeBinding`. Use @tybys/emnapi-node-binding package')
-      }
-      emnapiNodeBinding = nodeBinding
-    }
-
-    // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
-    const envObject = emnapiModule.envObject || (emnapiModule.envObject = emnapiCtx.createEnv(
-      (cb: Ptr) => $makeDynCall('vppp', 'cb'),
-      (cb: Ptr) => $makeDynCall('vp', 'cb')
-    ))
-
-    const scope = emnapiCtx.openScope(envObject)
-    try {
-      envObject.callIntoModule((_envObject) => {
-        const exports = emnapiModule.exports
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const exportsHandle = scope.add(exports)
-        const napiValue = _napi_register_wasm_v1($to64('_envObject.id'), $to64('exportsHandle.id'))
-        emnapiModule.exports = (!napiValue) ? exports : emnapiCtx.handleStore.get(napiValue)!.value
-      })
-    } catch (err) {
-      emnapiCtx.closeScope(envObject, scope)
-      throw err
-    }
-    emnapiCtx.closeScope(envObject, scope)
-    emnapiModule.loaded = true
-    delete emnapiModule.envObject
-    return emnapiModule.exports
-  }
+emnapiDefineVar('$emnapiCtx', undefined)
+emnapiDefineVar('$emnapiNodeBinding', undefined)
+emnapiDefineVar('$emnapiModule', {
+  exports: {},
+  loaded: false,
+  filename: ''
 })
+
+function emnapiInit (options: InitOptions): any {
+  if (emnapiModule.loaded) return emnapiModule.exports
+
+  if (typeof options !== 'object' || options === null) {
+    throw new TypeError('Invalid emnapi init option')
+  }
+
+  const context = options.context
+  if (typeof context !== 'object' || context === null) {
+    throw new TypeError("Invalid `options.context`. You can create a context by `import { createContext } from '@tybys/emnapi-runtime`'")
+  }
+
+  emnapiCtx = context
+
+  if (typeof options.filename === 'string') {
+    emnapiModule.filename = options.filename
+  }
+
+  if ('nodeBinding' in options) {
+    const nodeBinding = options.nodeBinding
+    if (typeof nodeBinding !== 'object' || nodeBinding === null) {
+      throw new TypeError('Invalid `options.nodeBinding`. Use @tybys/emnapi-node-binding package')
+    }
+    emnapiNodeBinding = nodeBinding
+  }
+
+  // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
+  const envObject = emnapiModule.envObject || (emnapiModule.envObject = emnapiCtx.createEnv(
+    (cb: Ptr) => $makeDynCall('vppp', 'cb'),
+    (cb: Ptr) => $makeDynCall('vp', 'cb')
+  ))
+
+  const scope = emnapiCtx.openScope(envObject)
+  try {
+    envObject.callIntoModule((_envObject) => {
+      const exports = emnapiModule.exports
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const exportsHandle = scope.add(exports)
+      const napiValue = _napi_register_wasm_v1($to64('_envObject.id'), $to64('exportsHandle.id'))
+      emnapiModule.exports = (!napiValue) ? exports : emnapiCtx.handleStore.get(napiValue)!.value
+    })
+  } catch (err) {
+    emnapiCtx.closeScope(envObject, scope)
+    throw err
+  }
+  emnapiCtx.closeScope(envObject, scope)
+  emnapiModule.loaded = true
+  delete emnapiModule.envObject
+  return emnapiModule.exports
+}
+
+emnapiImplementHelper(
+  '$emnapiInit',
+  undefined,
+  emnapiInit,
+  ['$emnapiModule', '$emnapiCtx', '$emnapiNodeBinding', 'napi_register_wasm_v1']
+)
--- packages/emnapi/src/util.ts
+++ packages/emnapi/src/util.ts
@@ -1,14 +1,11 @@
 /* eslint-disable @typescript-eslint/indent */
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-declare function _napi_set_last_error (env: napi_env, error_code: napi_status, engine_error_code: uint32_t, engine_reserved: void_p): napi_status
-
-function napi_set_last_error (env: napi_env, error_code: napi_status, engine_error_code: uint32_t, engine_reserved: void_p): napi_status {
+function _napi_set_last_error (env: napi_env, error_code: napi_status, engine_error_code: uint32_t, engine_reserved: void_p): napi_status {
   const envObject = emnapiCtx.envStore.get(env)!
   return envObject.setLastError(error_code, engine_error_code, engine_reserved)
 }
 
-function napi_clear_last_error (env: napi_env): napi_status {
+function _napi_clear_last_error (env: napi_env): napi_status {
   const envObject = emnapiCtx.envStore.get(env)!
   return envObject.clearLastError()
 }
@@ -34,22 +31,20 @@ function __emnapi_get_node_version (major: number, minor: number, patch: number)
   $makeSetValue('patch', 0, 'versions[2]', 'u32')
 }
 
-emnapiImplement('napi_set_last_error', 'ipiip', napi_set_last_error)
-emnapiImplement('napi_clear_last_error', 'ip', napi_clear_last_error)
+emnapiImplementInternal('napi_set_last_error', 'ipiip', _napi_set_last_error)
+emnapiImplementInternal('napi_clear_last_error', 'ip', _napi_clear_last_error)
 emnapiImplementInternal('_emnapi_get_node_version', 'vp', __emnapi_get_node_version)
 
 declare function runtimeKeepalivePush (): void
 declare function runtimeKeepalivePop (): void
 
-mergeInto(LibraryManager.library, {
-  _emnapi_runtime_keepalive_push__sig: 'v',
-  _emnapi_runtime_keepalive_push__deps: ['$runtimeKeepalivePush'],
-  _emnapi_runtime_keepalive_push: function () {
-    if (typeof runtimeKeepalivePush === 'function') runtimeKeepalivePush()
-  },
-  _emnapi_runtime_keepalive_pop__sig: 'v',
-  _emnapi_runtime_keepalive_pop__deps: ['$runtimeKeepalivePop'],
-  _emnapi_runtime_keepalive_pop: function () {
-    if (typeof runtimeKeepalivePop === 'function') runtimeKeepalivePop()
-  }
-})
+function __emnapi_runtime_keepalive_push (): void {
+  if (typeof runtimeKeepalivePush === 'function') runtimeKeepalivePush()
+}
+
+function __emnapi_runtime_keepalive_pop (): void {
+  if (typeof runtimeKeepalivePop === 'function') runtimeKeepalivePop()
+}
+
+emnapiImplementInternal('_emnapi_runtime_keepalive_push', 'v', __emnapi_runtime_keepalive_push, ['$runtimeKeepalivePush'])
+emnapiImplementInternal('_emnapi_runtime_keepalive_pop', 'v', __emnapi_runtime_keepalive_pop, ['$runtimeKeepalivePop'])
