c6d69681faa81a2422ce527563e15420d1991ac3
toyobayashi <lifenglin314@outlook.com>
2023-02-28 23:52:33 +0800

    handleMessage

--- packages/core/src/worker.js
+++ packages/core/src/worker.js
@@ -1,5 +1,5 @@
 export function handleMessage (e, callback) {
-  if (e.data.__emnapi__) {
+  if (e && e.data && e.data.__emnapi__) {
     const type = e.data.__emnapi__.type
     const payload = e.data.__emnapi__.payload
     callback(type, payload)
