87429a41ee838a95d6db13890793e111cceb4515
toyobayashi <lifenglin314@outlook.com>
2023-03-12 14:38:22 +0800

    move

--- packages/emnapi/src/thread-safe-function.ts
+++
@@ -1,7 +0,0 @@
-function __emnapi_call_finalizer (env: napi_env, callback: number, data: number, hint: number): void {
-  const envObject = emnapiCtx.envStore.get(env)!
-  $from64('callback')
-  envObject.callFinalizer(callback, data, hint)
-}
-
-emnapiImplementInternal('_emnapi_call_finalizer', 'vpppp', __emnapi_call_finalizer)
--- packages/emnapi/src/util.ts
+++ packages/emnapi/src/util.ts
@@ -45,3 +45,54 @@ function __emnapi_runtime_keepalive_pop (): void {
 
 emnapiImplementInternal('_emnapi_runtime_keepalive_push', 'v', __emnapi_runtime_keepalive_push, ['$runtimeKeepalivePush'])
 emnapiImplementInternal('_emnapi_runtime_keepalive_pop', 'v', __emnapi_runtime_keepalive_pop, ['$runtimeKeepalivePop'])
+
+function __emnapi_set_immediate (callback: number, data: number): void {
+  emnapiCtx.feature.setImmediate(() => {
+    $makeDynCall('vp', 'callback')(data)
+  })
+}
+
+function __emnapi_next_tick (callback: number, data: number): void {
+  // eslint-disable-next-line @typescript-eslint/no-floating-promises
+  Promise.resolve().then(() => {
+    $makeDynCall('vp', 'callback')(data)
+  })
+}
+
+function __emnapi_call_into_module (env: napi_env, callback: number, data: number, close_scope_if_throw: int): void {
+  const envObject = emnapiCtx.envStore.get(env)!
+  const scope = emnapiCtx.openScope(envObject)
+  try {
+    envObject.callIntoModule(() => {
+      $makeDynCall('vpp', 'callback')(env, data)
+    })
+  } catch (err) {
+    emnapiCtx.closeScope(envObject, scope)
+    if (close_scope_if_throw) {
+      emnapiCtx.closeScope(envObject)
+    }
+    throw err
+  }
+  emnapiCtx.closeScope(envObject, scope)
+}
+
+function __emnapi_call_finalizer (env: napi_env, callback: number, data: number, hint: number): void {
+  const envObject = emnapiCtx.envStore.get(env)!
+  $from64('callback')
+  envObject.callFinalizer(callback, data, hint)
+}
+
+function __emnapi_ctx_increase_waiting_request_counter (): void {
+  emnapiCtx.increaseWaitingRequestCounter()
+}
+
+function __emnapi_ctx_decrease_waiting_request_counter (): void {
+  emnapiCtx.decreaseWaitingRequestCounter()
+}
+
+emnapiImplementInternal('_emnapi_set_immediate', 'vpp', __emnapi_set_immediate)
+emnapiImplementInternal('_emnapi_next_tick', 'vpp', __emnapi_next_tick)
+emnapiImplementInternal('_emnapi_call_into_module', 'vpppi', __emnapi_call_into_module)
+emnapiImplementInternal('_emnapi_call_finalizer', 'vpppp', __emnapi_call_finalizer)
+emnapiImplementInternal('_emnapi_ctx_increase_waiting_request_counter', 'v', __emnapi_ctx_increase_waiting_request_counter)
+emnapiImplementInternal('_emnapi_ctx_decrease_waiting_request_counter', 'v', __emnapi_ctx_decrease_waiting_request_counter)
