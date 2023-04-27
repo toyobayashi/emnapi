6f2691d581fafcba83e68cf4a0f31cb2d806e357
toyobayashi <lifenglin314@outlook.com>
2023-04-28 00:16:59 +0800

    test: passing NULL to napi_define_class

---
+++ packages/test/constructor/constructor-null.test.js
@@ -0,0 +1,20 @@
+/* eslint-disable camelcase */
+'use strict'
+const assert = require('assert')
+const { load } = require('../util')
+
+// Test passing NULL to object-related N-APIs.
+module.exports = load('constructor').then(({ testNull }) => {
+  const expectedResult = {
+    envIsNull: 'Invalid argument',
+    nameIsNull: 'Invalid argument',
+    lengthIsZero: 'napi_ok',
+    nativeSideIsNull: 'Invalid argument',
+    dataIsNull: 'napi_ok',
+    propsLengthIsZero: 'napi_ok',
+    propsIsNull: 'Invalid argument',
+    resultIsNull: 'Invalid argument'
+  }
+
+  assert.deepStrictEqual(testNull.testDefineClass(), expectedResult)
+})
