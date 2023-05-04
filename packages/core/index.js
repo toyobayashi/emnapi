54d49da54f487d54e2bdf4f48f93285c01c68fa5
toyobayashi <lifenglin314@outlook.com>
2023-05-04 13:39:00 +0800

    check process in entry

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV === 'production') {
+if (typeof process !== 'undefined' && process.env.NODE_ENV === 'production') {
   module.exports = require('./dist/emnapi-core.cjs.min.js')
 } else {
   module.exports = require('./dist/emnapi-core.cjs.js')
