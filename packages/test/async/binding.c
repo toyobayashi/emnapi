fdd107c7647d9c3dfb86e6d648ded33fe8c9b226
toyobayashi <lifenglin314@outlook.com>
2023-08-17 21:11:48 +0800

    test: fix assertion message in test_async.c

--- packages/test/async/binding.c
+++ packages/test/async/binding.c
@@ -83,7 +83,7 @@ static napi_value Test(napi_env env, napi_callback_info info) {
   void* data;
   NAPI_CALL(env,
     napi_get_cb_info(env, info, &argc, argv, &_this, &data));
-  NAPI_ASSERT(env, argc >= 3, "Not enough arguments, expected 2.");
+  NAPI_ASSERT(env, argc >= 3, "Not enough arguments, expected 3.");
 
   napi_valuetype t;
   NAPI_CALL(env, napi_typeof(env, argv[0], &t));
