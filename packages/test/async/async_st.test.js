df478b226d4893cbcc12185b1e603a08c1834fe6
toyobayashi <lifenglin314@outlook.com>
2023-11-28 13:03:35 +0800

    fix single thread async work and tsfn test

--- packages/test/async/async_st.test.js
+++ packages/test/async/async_st.test.js
@@ -3,4 +3,7 @@
 const { load } = require('../util')
 const main = require('./main')
 
-module.exports = main(load('async_st'), __filename)
+const RUNTIME_UV_THREADPOOL_SIZE = ('UV_THREADPOOL_SIZE' in process.env) ? Number(process.env.UV_THREADPOOL_SIZE) : 4
+module.exports = main(load('async_st', {
+  asyncWorkPoolSize: -RUNTIME_UV_THREADPOOL_SIZE
+}), __filename)
--- packages/test/tsfn2/tsfn2_st.test.js
+++ packages/test/tsfn2/tsfn2_st.test.js
@@ -3,4 +3,9 @@
 const { load } = require('../util')
 const main = require('./main')
 
-module.exports = main(load('tsfn2_st', { nodeBinding: require('@emnapi/node-binding') }))
+const RUNTIME_UV_THREADPOOL_SIZE = ('UV_THREADPOOL_SIZE' in process.env) ? Number(process.env.UV_THREADPOOL_SIZE) : 4
+
+module.exports = main(load('tsfn2_st', {
+  nodeBinding: require('@emnapi/node-binding'),
+  asyncWorkPoolSize: -RUNTIME_UV_THREADPOOL_SIZE
+}))
