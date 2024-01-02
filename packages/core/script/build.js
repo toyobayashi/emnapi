cd6b4bd58f4c094318c980f3ed3839e61a70cc9f
toyobayashi <lifenglin314@outlook.com>
2024-01-02 12:02:09 +0800

    refactor: migrate `@emnapi/core` to typescript

--- packages/core/script/build.js
+++ packages/core/script/build.js
@@ -6,9 +6,28 @@ const rollupTypescript = require('@rollup/plugin-typescript').default
 const rollupNodeResolve = require('@rollup/plugin-node-resolve').default
 const rollupReplace = require('@rollup/plugin-replace').default
 const rollupTerser = require('@rollup/plugin-terser').default
+const rollupAlias = require('@rollup/plugin-alias').default
+const { compile } = require('@tybys/tsapi')
+const { globSync } = require('glob')
 const dist = path.join(__dirname, '../dist')
 
 function build () {
+  compile(path.join(__dirname, '../tsconfig.json'), {
+    optionsToExtend: {
+      target: require('typescript').ScriptTarget.ES2019,
+      emitDeclarationOnly: true,
+      declaration: true,
+      declarationDir: path.join(__dirname, '../lib/typings')
+    }
+  })
+
+  globSync('**/*.d.ts', { cwd: path.join(__dirname, '../src/emnapi') }).forEach(file => {
+    const from = path.join(path.join(__dirname, '../src/emnapi', file))
+    const to = path.join(path.join(__dirname, '../lib/typings/emnapi', file))
+    fs.mkdirSync(path.dirname(to), { recursive: true })
+    fs.copyFileSync(from, to)
+  })
+
   /**
    * @param {ts.ScriptTarget} esversion
    * @param {boolean=} minify
@@ -16,7 +35,7 @@ function build () {
    */
   function createInput (esversion, minify, external) {
     return {
-      input: path.join(__dirname, '../src/index.js'),
+      input: path.join(__dirname, '../src/index.ts'),
       external,
       plugins: [
         rollupTypescript({
@@ -37,6 +56,11 @@ function build () {
               require('@tybys/ts-transform-pure-class').default
             ]
           }
+        }),
+        rollupAlias({
+          entries: [
+            { find: '@', replacement: path.join(__dirname, '../src') }
+          ]
         }),
         rollupNodeResolve({
           mainFields: ['module', 'main']
@@ -136,16 +160,32 @@ function build () {
   ]).map(conf => {
     return rollup.rollup(conf.input).then(bundle => bundle.write(conf.output))
   })).then(() => {
-    const dts = path.join(__dirname, '../index.d.ts')
-    const dest = path.join(__dirname, '../dist/emnapi-core.d.ts')
+    const {
+      Extractor,
+      ExtractorConfig
+    } = require('@microsoft/api-extractor')
+    const apiExtractorJsonPath = path.join(__dirname, '../api-extractor.json')
+    const extractorConfig = ExtractorConfig.loadFileAndPrepare(apiExtractorJsonPath)
+    const extractorResult = Extractor.invoke(extractorConfig, {
+      localBuild: true,
+      showVerboseMessages: true
+    })
+    if (extractorResult.succeeded) {
+      console.log('API Extractor completed successfully')
+    } else {
+      const errmsg = `API Extractor completed with ${extractorResult.errorCount} errors and ${extractorResult.warningCount} warnings`
+      return Promise.reject(new Error(errmsg))
+    }
+
+    const dts = extractorConfig.publicTrimmedFilePath
+
     const mDts = path.join(__dirname, '../dist/emnapi-core.d.mts')
     const cjsMinDts = path.join(__dirname, '../dist/emnapi-core.cjs.min.d.ts')
     const mjsMinDts = path.join(__dirname, '../dist/emnapi-core.min.d.mts')
-    fs.copyFileSync(dts, dest)
     fs.copyFileSync(dts, mDts)
     fs.copyFileSync(dts, cjsMinDts)
     fs.copyFileSync(dts, mjsMinDts)
-    fs.appendFileSync(dest, '\nexport as namespace emnapiCore;\n', 'utf8')
+    fs.appendFileSync(dts, '\nexport as namespace emnapiCore;\n', 'utf8')
   })
 }
 
---
+++ packages/core/src/index.ts
@@ -0,0 +1,38 @@
+export { createNapiModule } from './emnapi/index'
+export {
+  loadNapiModule,
+  loadNapiModuleSync,
+  instantiateNapiModule,
+  instantiateNapiModuleSync
+} from './load'
+
+export { MessageHandler } from './worker'
+
+declare const __VERSION__: string
+export const version = __VERSION__
+
+export type {
+  PointerInfo,
+  InitOptions,
+  NapiModule,
+  NodeBinding,
+  CreateWorkerInfo,
+  BaseCreateOptions,
+  CreateOptions
+} from './emnapi/index'
+
+export type {
+  LoadOptions,
+  InstantiateOptions,
+  InstantiatedSource,
+  ReactorWASI
+} from './load'
+
+export type {
+  OnLoadData,
+  HandleOptions
+} from './worker'
+
+export type {
+  InputType
+} from './util'
