3c126e3bf5fbc1aad36c995aa1699996a7660c65
toyobayashi <lifenglin314@outlook.com>
2024-12-13 16:41:57 +0800

    fix

--- packages/test/objwrap/objwrapbasicfinalizer.test.js
+++ packages/test/objwrap/objwrapbasicfinalizer.test.js
@@ -2,18 +2,23 @@
 /* eslint-disable camelcase */
 'use strict'
 const { load } = require('../util')
-const common = require('../common')
-const { onGC, gcUntil } = require('../gc')
+const assert = require('assert')
+const { gcUntil } = require('../gc')
 
 const p = load('objwrapbasicfinalizer')
 module.exports = p.then(async addon => {
-  let obj = new addon.MyObject(9)
-  let called = false
-  onGC(obj, {
-    ongc: common.mustCall(() => {
-      called = true
-    })
+  // This test verifies that ObjectWrap can be correctly finalized with a node_api_basic_finalizer
+  // in the current JS loop tick
+  (() => {
+    let obj = new addon.MyObject(9)
+    obj = null
+    // Silent eslint about unused variables.
+    assert.strictEqual(obj, null)
+  })()
+
+  await gcUntil(() => {
+    return addon.getFinalizerCallCount() === 1
   })
-  obj = null
-  await gcUntil(() => called)
+
+  assert.strictEqual(addon.getFinalizerCallCount(), 1)
 })
