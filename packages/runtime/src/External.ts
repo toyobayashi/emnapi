d0fd7300ca3029e891d74d98dd51164303c8f9ef
toyobayashi <lifenglin314@outlook.com>
2025-01-22 19:09:39 +0800

    export

--- packages/runtime/src/External.ts
+++ packages/runtime/src/External.ts
@@ -1,15 +1,16 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+/** @public */
 export interface External extends Record<any, any> {}
 
 const externalValue = new WeakMap<External, number | bigint>()
 
+/** @public */
 export function isExternal (object: unknown): object is External {
   return externalValue.has(object as any)
 }
 
-// eslint-disable-next-line @typescript-eslint/no-redeclare
+/** @public */ // eslint-disable-next-line @typescript-eslint/no-redeclare
 export const External = (() => {
-  function External (this: any, value: number | bigint): void {
+  function External (this: External, value: number | bigint): void {
     Object.setPrototypeOf(this, null)
     externalValue.set(this, value)
   }
@@ -20,6 +21,7 @@ export const External = (() => {
   }
 })()
 
+/** @public */
 export function getExternalValue (external: External): number | bigint {
   if (!isExternal(external)) {
     throw new TypeError('not external')
