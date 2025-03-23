e6248e2fd2bf56f69dccae3dca8662f8eacc308c
toyobayashi <lifenglin314@outlook.com>
2025-03-24 00:36:11 +0800

    wip

--- packages/rollup-plugin-emscripten-esm-library/rollup.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from '@emnapi/shared'
-
-export default defineConfig({
-  outputFile: 'index',
-  browser: false,
-  sourcemap: true,
-  external: ['@emnapi/ts-transform-emscripten-esm-library']
-})
