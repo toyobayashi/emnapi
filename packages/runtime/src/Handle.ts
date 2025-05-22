3211df95dd0157a5a340eb53424bf79d8c5b8d62
toyobayashi <lifenglin314@outlook.com>
2025-05-22 11:02:21 +0800

    fix

--- packages/runtime/src/Handle.ts
+++ packages/runtime/src/Handle.ts
@@ -60,7 +60,7 @@ export class HandleStore extends BaseArrayStore<any> {
     this._deref = this._features.finalizer
       ? (id: number | bigint): any => {
           const value = this._values[id as number]
-          if (value instanceof WeakRef && id >= this._allocator.next) {
+          if (value instanceof WeakRef && Number(id) >= this._allocator.next) {
             return value.deref()
           }
           return value
