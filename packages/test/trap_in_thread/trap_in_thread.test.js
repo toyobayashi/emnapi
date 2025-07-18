9317d2f64444ed6fa7413028f721cde9473d5eb6
toyobayashi <lifenglin314@outlook.com>
2025-07-18 15:58:31 +0800

    test

--- packages/test/trap_in_thread/trap_in_thread.test.js
+++ packages/test/trap_in_thread/trap_in_thread.test.js
@@ -10,21 +10,37 @@ async function main () {
     return
   }
 
-  const test = (f, signal) => {
-    const { spawnSync } = require('child_process')
-    const child = spawnSync(process.execPath, [
+  const test = async (f, signal) => {
+    const { spawn } = require('child_process')
+    const child = spawn(process.execPath, [
       '--expose-gc',
       ...(process.env.EMNAPI_TEST_WASI ? ['--experimental-wasi-unstable-preview1'] : []),
       ...(process.env.MEMORY64 ? ['--experimental-wasm-memory64'] : []),
       __filename,
       'child', f
-    ])
-    assert.strictEqual(child.signal, process.env.EMNAPI_TEST_NATIVE ? signal : null)
+    ], { stdio: 'inherit' })
+    const result = await new Promise((resolve, reject) => {
+      let exit = false
+      child.on('exit', (code, signal) => {
+        console.log(`Child exited with code: ${code}, signal: ${signal}`)
+        exit = true
+        resolve({
+          code,
+          signal
+        })
+      })
+      setTimeout(() => {
+        if (exit) return
+        reject(new Error(`Test timed out: ${f}`))
+        child.kill('SIGKILL')
+      }, 2000)
+    })
+    assert.strictEqual(result.signal, process.env.EMNAPI_TEST_NATIVE ? signal : null)
   }
 
-  test('abort', 'SIGABRT')
-  test('releaseInThread', null)
-  test('abortInThread','SIGABRT')
+  await test('abort', 'SIGABRT')
+  await test('releaseInThread', null)
+  await test('abortInThread','SIGABRT')
 }
 
 module.exports = main()
