58068a578c224fb8726077fdb7eafaa882efc054
toyobayashi <lifenglin314@outlook.com>
2025-08-08 11:33:19 +0800

    pthread cancel

--- packages/emnapi/src/wasi_wait.c
+++ packages/emnapi/src/wasi_wait.c
@@ -6,6 +6,17 @@
 #include <stdio.h>
 #include "emnapi_internal.h"
 
+struct __pthread {
+  unsigned char _[32];
+  volatile int cancel;
+  volatile unsigned char canceldisable, cancelasync;
+	unsigned char tsd_used:1;
+	unsigned char dlerror_flag:1;
+  unsigned char __[68];
+};
+
+#define INFINITY __builtin_inff()
+
 int __wasilibc_futex_wait_atomic_wait(volatile void *addr, int op, int val, int64_t max_wait_ns);
 
 static _Atomic pthread_t crashed_thread_id = NULL;
@@ -22,36 +33,14 @@ void _emnapi_yield() {
   }
 }
 
-int _emnapi_wait(int is_runtime_thread, volatile void *addr, int op, int val, double max_wait_ms) {
-  if (is_runtime_thread) {
-    _emnapi_yield();
-  }
-
-  int64_t max_wait_ns = -1;
-  if (max_wait_ms != __builtin_inff()) {
-    max_wait_ns = (int64_t)(max_wait_ms*1000*1000);
-  }
-
-  // https://github.com/WebAssembly/wasi-libc/blob/3f7eb4c7d6ede4dde3c4bffa6ed14e8d656fe93f/libc-top-half/musl/src/thread/wasm32/__wasilibc_busywait.c#L42
-  if (!_emnapi_is_main_browser_thread()) {
-    return __wasilibc_futex_wait_atomic_wait(addr, op, val, max_wait_ns);
-  }
-
-  struct timespec start;
-  int r = clock_gettime(CLOCK_REALTIME, &start);
-
-  if (r) return r;
+static int _emnapi_wait_main_browser_thread(int is_runtime_thread, volatile void *addr, int op, int val, double max_wait_ms) {
+  double now = _emnapi_get_now();
+  double end = now + max_wait_ms;
 
   while (1) {
-    if (max_wait_ns >= 0) {
-      struct timespec now;
-      r = clock_gettime(CLOCK_REALTIME, &now);
-      if (r) return r;
-
-      int64_t elapsed_ns = (now.tv_sec - start.tv_sec) * 1000000000 + now.tv_nsec - start.tv_nsec;
-      if (elapsed_ns >= max_wait_ns) {
-        return -ETIMEDOUT;
-      }
+    now = _emnapi_get_now();
+    if (now >= end) {
+      return -ETIMEDOUT;
     }
 
     if (is_runtime_thread) {
@@ -66,15 +55,39 @@ int _emnapi_wait(int is_runtime_thread, volatile void *addr, int op, int val, do
   return 0;
 }
 
+int _emnapi_wait(int is_runtime_thread, volatile void *addr, int op, int val, double max_wait_ms) {
+  if (is_runtime_thread) {
+    _emnapi_yield();
+  }
+
+  // https://github.com/WebAssembly/wasi-libc/blob/3f7eb4c7d6ede4dde3c4bffa6ed14e8d656fe93f/libc-top-half/musl/src/thread/wasm32/__wasilibc_busywait.c#L42
+  if (!_emnapi_is_main_browser_thread()) {
+    int64_t max_wait_ns = -1;
+    if (max_wait_ms != INFINITY) {
+      max_wait_ns = (int64_t)(max_wait_ms*1000*1000);
+    }
+    return __wasilibc_futex_wait_atomic_wait(addr, op, val, max_wait_ns);
+  }
+
+  return _emnapi_wait_main_browser_thread(is_runtime_thread, addr, op, val, max_wait_ms);
+}
+
 int __wasilibc_futex_wait_maybe_busy(volatile void *addr, int op, int val, int64_t max_wait_ns) {
+  // https://github.com/emscripten-core/emscripten/blob/89ce854a99238d04116a3d9b5afe241eec90c6c3/system/lib/libc/musl/src/thread/__timedwait.c#L60
   int r = 0;
-  double msecsToSleep = max_wait_ns >= 0 ? (max_wait_ns / 1000000.0) : __builtin_inff();
+  double msecsToSleep = max_wait_ns >= 0 ? (max_wait_ns / 1000000.0) : INFINITY;
   int is_runtime_thread = _emnapi_is_main_runtime_thread();
 	double max_ms_slice_to_sleep = is_runtime_thread ? 1 : 100;
 
-  if (is_runtime_thread) {
+  if (is_runtime_thread ||
+	    pthread_self()->canceldisable != PTHREAD_CANCEL_DISABLE ||
+	    pthread_self()->cancelasync) {
     double sleepUntilTime = _emnapi_get_now() + msecsToSleep;
 		do {
+      if (pthread_self()->cancel) {
+				pthread_testcancel();
+				return ECANCELED;
+			}
 			msecsToSleep = sleepUntilTime - _emnapi_get_now();
 			if (msecsToSleep <= 0) {
 				r = ETIMEDOUT;
