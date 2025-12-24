68bfcbfca60e525f5bf9fbe90def5f70aebb6325
toyobayashi <lifenglin314@outlook.com>
2025-12-24 11:37:46 +0800

    refactor: make tsfn js plugin

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from '@emnapi/shared'
 import pkg from './package.json' with { type: 'json' }
-import { join } from 'path'
+import { join, dirname } from 'path'
+import fs from 'fs'
+
+const plugins = [
+  ['v8', 'v8', 'emnapiCorePluginsV8'],
+  ['asyncWork', 'async-work', 'emnapiCorePluginsAsyncWork'],
+  ['tsfn', 'threadsafe-function', 'emnapiCorePluginsThreadSafeFunction']
+]
 
 export default [
   ...defineConfig({
@@ -8,30 +15,39 @@ export default [
     outputFile: 'emnapi-core',
     defines: {
       __VERSION__: JSON.stringify(pkg.version)
-    }
+    },
+    plugins: [
+      {
+        name: 'rollup-plugin-create-plugin-entry',
+        writeBundle (outputOptions) {
+          const distDir = dirname(outputOptions.file)
+          fs.mkdirSync(join(distDir, 'plugins'), { recursive: true })
+          const entryFile = join(distDir, 'plugins', 'index.js')
+          const dtsEntryFile = join(distDir, 'plugins', 'index.d.ts')
+          let content = ''
+          for (const [name, file] of plugins) {
+            content += `export { default as ${name} } from './${file}.js'\n`
+          }
+          let dtsContent = "export { PluginFactory, PluginContext, EmnapiPlugin } from '../emnapi-core'\n"
+          for (const [name, file] of plugins) {
+            dtsContent += `export { default as ${name} } from './${file}'\n`
+          }
+          fs.writeFileSync(entryFile, content, 'utf8')
+          fs.writeFileSync(dtsEntryFile, dtsContent, 'utf8')
+        }
+      }
+    ]
   }),
-  ...defineConfig({
-    outputName: 'emnapiCorePluginsV8',
-    outputFile: 'plugins/v8',
-    dtsEntry: 'dist/types/emnapi/v8.d.ts',
+  ...plugins.flatMap(([_, name, outputName]) => defineConfig({
+    outputName,
+    outputFile: `plugins/${name}`,
+    dtsEntry: `dist/types/emnapi/${name}.d.ts`,
     staticDtsContent: (code) => code.replace(/\.\/index/g, '../emnapi-core'),
-    input: join(import.meta.dirname, 'src/emnapi/v8.js'),
+    input: join(import.meta.dirname, `src/emnapi/${name}.js`),
     compilerOptions: {
       declaration: false,
       declarationMap: false,
       declarationDir: undefined
     }
-  }),
-  ...defineConfig({
-    outputName: 'emnapiCorePluginsAsyncWork',
-    outputFile: 'plugins/async-work',
-    dtsEntry: 'dist/types/emnapi/async-work.d.ts',
-    staticDtsContent: (code) => code.replace(/\.\/index/g, '../emnapi-core'),
-    input: join(import.meta.dirname, 'src/emnapi/async-work.js'),
-    compilerOptions: {
-      declaration: false,
-      declarationMap: false,
-      declarationDir: undefined
-    }
-  })
+  }))
 ]
