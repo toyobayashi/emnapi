4fad89eb32c9f04370d48f7caf502205a63e82ab
Toyo Li <lifenglin314@outlook.com>
2026-01-04 18:00:34 +0800

    [Backport] feat: add support for Float16Array (#191)

--- packages/emnapi/src/value/convert2c.ts
+++ packages/emnapi/src/value/convert2c.ts
@@ -134,6 +134,8 @@ export function napi_get_typedarray_info (
       t = napi_typedarray_type.napi_int32_array
     } else if (v instanceof Uint32Array) {
       t = napi_typedarray_type.napi_uint32_array
+    } else if (typeof Float16Array === 'function' && v instanceof Float16Array) {
+      t = napi_typedarray_type.napi_float16_array
     } else if (v instanceof Float32Array) {
       t = napi_typedarray_type.napi_float32_array
     } else if (v instanceof Float64Array) {
@@ -213,7 +215,7 @@ export function napi_get_dataview_info (
   if (!handle.isDataView()) {
     return envObject.setLastError(napi_status.napi_invalid_arg)
   }
-  const v = handle.value as DataView
+  const v = handle.value as DataView<ArrayBufferLike>
   if (byte_length) {
     from64('byte_length')
     makeSetValue('byte_length', 0, 'v.byteLength', SIZE_TYPE)
