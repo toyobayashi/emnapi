a64942ad2af7b441a96f88cc1a70c05e8f554b32
toyobayashi <lifenglin314@outlook.com>
2026-02-24 15:48:04 +0800

    add 'tsfn_shutdown/**/*' to pthread ignore list

--- packages/test/script/test.js
+++ packages/test/script/test.js
@@ -17,6 +17,7 @@ const pthread = [
   'node-addon-api/**/*',
   'pool/**/*',
   'tsfn/**/*',
+  'tsfn_shutdown/**/*',
   'async_cleanup_hook/**/*',
   'string/string-pthread.test.js',
   'uv_threadpool_size/**/*',
