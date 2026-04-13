c21f51bcc2d2ae07f2197d386fc05918baaa312c
Toyo Li <lifenglin314@outlook.com>
2026-04-13 18:53:53 +0800

    fix!: `napi_adjust_external_memory` no longer grow wasm memory (#207)

--- packages/emnapi/src/core/memory.ts
+++ packages/emnapi/src/core/memory.ts
@@ -1,5 +1,4 @@
 import { emnapiCtx, emnapiEnv } from 'emnapi:shared'
-import { wasmMemory } from 'emscripten:runtime'
 import { from64, makeSetValue } from 'emscripten:parse-tools'
 import { $CHECK_ENV, $CHECK_ARG } from '../macro'
 import { $emnapiSetValueI64 as emnapiSetValueI64 } from '../util'
@@ -14,24 +13,13 @@ export function napi_adjust_external_memory (
   const envObject = emnapiEnv
   $CHECK_ARG!(envObject, adjusted_value)
 
-  const change_in_bytes_number = Number(change_in_bytes)
-
-  if (change_in_bytes_number < 0) {
-    return envObject.setLastError(napi_status.napi_invalid_arg)
-  }
-
-  const old_size = wasmMemory.buffer.byteLength
-  let new_size = old_size + change_in_bytes_number
-  new_size = new_size + ((65536 - new_size % 65536) % 65536)
-  if (wasmMemory.grow((new_size - old_size + 65535) >> 16) === -1) {
-    return envObject.setLastError(napi_status.napi_generic_failure)
-  }
+  let adjusted_memory = emnapiCtx.adjustAmountOfExternalAllocatedMemory(change_in_bytes)
 
   from64('adjusted_value')
   if (emnapiCtx.features.BigInt) {
-    makeSetValue('adjusted_value', 0, 'wasmMemory.buffer.byteLength', 'i64')
+    makeSetValue('adjusted_value', 0, 'adjusted_memory', 'i64')
   } else {
-    emnapiSetValueI64(adjusted_value, wasmMemory.buffer.byteLength)
+    emnapiSetValueI64(adjusted_value, Number(adjusted_memory))
   }
 
   return envObject.clearLastError()
