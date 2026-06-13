2b1efde46351b6eb7959ab9b75c088fc6a0177e0
toyobayashi <lifenglin314@outlook.com>
2026-06-13 11:19:33 +0800

    more ensure buffer

--- packages/emnapi/src/core/async-work.ts
+++ packages/emnapi/src/core/async-work.ts
@@ -158,18 +158,23 @@ var emnapiAWMT = {
     return emnapiAWMT.workerReady as Promise<any>
   },
   getResource (work: number): number {
+    emnapiAWMT.ensureBufferFor(work + emnapiAWMT.offset.resource + 4)
     return makeGetValue('work', 'emnapiAWMT.offset.resource', '*')
   },
   getExecute (work: number): number {
+    emnapiAWMT.ensureBufferFor(work + emnapiAWMT.offset.execute + 4)
     return makeGetValue('work', 'emnapiAWMT.offset.execute', '*')
   },
   getComplete (work: number): number {
+    emnapiAWMT.ensureBufferFor(work + emnapiAWMT.offset.complete + 4)
     return makeGetValue('work', 'emnapiAWMT.offset.complete', '*')
   },
   getEnv (work: number): number {
+    emnapiAWMT.ensureBufferFor(work + emnapiAWMT.offset.env + 4)
     return makeGetValue('work', 'emnapiAWMT.offset.env', '*')
   },
   getData (work: number): number {
+    emnapiAWMT.ensureBufferFor(work + emnapiAWMT.offset.data + 4)
     return makeGetValue('work', 'emnapiAWMT.offset.data', '*')
   },
   getMutex () {
@@ -235,10 +240,13 @@ var emnapiAWMT = {
     return cond
   },
   queueInit (q: number) {
+    emnapiAWMT.ensureBufferFor(q + POINTER_SIZE + 4)
     makeSetValue('q', 0, 'q', '*')
     makeSetValue('q', POINTER_SIZE, 'q', '*')
   },
   queueInsertTail (h: number, q: number): void {
+    emnapiAWMT.ensureBufferFor(h + POINTER_SIZE + 4)
+    emnapiAWMT.ensureBufferFor(q + POINTER_SIZE + 4)
     makeSetValue('q', 0, 'h', '*')
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const tempValue = makeGetValue('h', POINTER_SIZE, '*')
@@ -249,6 +257,7 @@ var emnapiAWMT = {
     makeSetValue('h', POINTER_SIZE, 'q', '*')
   },
   queueRemove (q: number): void {
+    emnapiAWMT.ensureBufferFor(q + POINTER_SIZE + 4)
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const qprev = makeGetValue('q', POINTER_SIZE, '*')
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -257,6 +266,7 @@ var emnapiAWMT = {
     makeSetValue('qnext', POINTER_SIZE, 'qprev', '*')
   },
   queueEmpty (q: number): boolean {
+    emnapiAWMT.ensureBufferFor(q + 4)
     // eslint-disable-next-line eqeqeq
     return q == makeGetValue('q', 0, '*')
   },
@@ -287,6 +297,7 @@ var emnapiAWMT = {
       throw err
     }
 
+    emnapiAWMT.ensureBufferFor(emnapiAWMT.globalAddress + emnapiAWMT.globalOffset.idle_threads + 4)
     if (makeGetValue('emnapiAWMT.globalAddress', 'emnapiAWMT.globalOffset.idle_threads', 'u32') > 0) {
       cond.signal()
     }
@@ -295,6 +306,7 @@ var emnapiAWMT = {
   cancelWork (work: number) {
     let cancelled = false
     emnapiAWMT.getMutex().execute(() => {
+      emnapiAWMT.ensureBufferFor(work + emnapiAWMT.offset.status + 4)
       cancelled = !emnapiAWMT.queueEmpty(work + emnapiAWMT.offset.queue) && makeGetValue('work', 'emnapiAWMT.offset.status', 'i32') !== AsyncWorkStatus.Completed
       if (cancelled) {
         emnapiAWMT.queueRemove(work + emnapiAWMT.offset.queue)
@@ -485,6 +497,7 @@ export function _emnapi_async_worker (globalAddress: number): number {
   const idleThreadsAddr = globalAddress + emnapiAWMT.globalOffset.idle_threads
   const workerQueueAddr = globalAddress + emnapiAWMT.globalOffset.q
   for (;;) {
+    emnapiAWMT.ensureBufferFor(workerQueueAddr + 4)
     while (emnapiAWMT.queueEmpty(workerQueueAddr)) {
       Atomics.add(new Int32Array(emnapiAWMT.ensureBufferFor(idleThreadsAddr + 4), idleThreadsAddr, 1), 0, 1)
       cond.wait()
--- packages/emnapi/src/threadsafe-function.ts
+++ packages/emnapi/src/threadsafe-function.ts
@@ -515,6 +515,7 @@ export const emnapiTSFN = {
       }
       const resource = emnapiTSFN.getResource(func)
       emnapiCtx.refStore.get(resource)!.dispose()
+      emnapiTSFN.ensureBufferFor(func + emnapiTSFN.offset.is_some + 1)
       makeSetValue('func', 'emnapiTSFN.offset.is_some', '0', 'i8')
 
       emnapiCtx.removeCleanupHook(envObject, emnapiTSFN.cleanup, func)
